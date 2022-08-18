02e716c5eddfdf14aa0d40c87722810b285f5118
a2407k <109984339+a2407k@users.noreply.github.com>
2022-08-18 19:20:58 +0530

    Create StringSplit.java

---
+++ StringSplit.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringSplit {
+
+	public static List<String> splitString(String str, char esc) {
+		List<String> result = new ArrayList<String>();
+
+		// Your Logic goes here
+
+		return result;
+	}
+
+	public static void main(String[] args) {
+		String str = "My name is 'Ashok Kumar Gowda' I have 12+ years of experience I work for 'IBM India Pvt Ltd' ";
+
+		List<String> result = splitString(str, '\'');
+
+		for (String a : result) {
+			System.out.println(a);
+		}
+	}
+}
