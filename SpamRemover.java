3ade442d30f1e8f87a6957e4839605892f62c43d
a2407k <109984339+a2407k@users.noreply.github.com>
2022-08-18 19:17:11 +0530

    Create SpamRemover.java

---
+++ SpamRemover.java
@@ -0,0 +1,15 @@
+public class SpamRemover {
+
+	public static void removeSpam(String folder) {
+
+		// Your logic goes here
+    
+	}
+
+	public static void main(String[] args) throws Exception {
+		String folder = "C:/notes";
+
+		removeSpam(folder);
+	}
+
+}
